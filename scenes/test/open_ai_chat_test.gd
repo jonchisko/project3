dc300a2caa6e1cbff7139491b7872148385ddcd6
Jon Škoberne <jonchisko@gmail.com>
2025-02-12 11:26:57 +0100

    Add test scene for chat manager

---
+++ scenes/test/open_ai_chat_test.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var _button: Button = %Button
+@onready var _line_edit: LineEdit = %LineEdit
+@onready var interactable_npc = $InteractableNpc
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self._button.disabled = self._line_edit.text.is_empty()
+
+
+func _on_button_pressed():
+	GameEvents.initiate_npc_interaction.emit(interactable_npc as InteractableNpc)
+
+
+func _on_line_edit_text_submitted(new_text):
+	if self._button.disabled:
+		self._button.disabled = false
+		
+	ChatManager.ApiKey = self._line_edit.text
