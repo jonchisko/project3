1bf4713fbbb8d8cf8b07968a03129054763a0c29
Jon Škoberne <jonchisko@gmail.com>
2025-03-12 17:52:47 +0100

    Fix open ai chat test scene

--- scenes/test/open_ai_chat_test.gd
+++ scenes/test/open_ai_chat_test.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var chatManager: ChatManager
+
 @onready var _button: Button = %Button
 @onready var _line_edit: LineEdit = %LineEdit
 @onready var interactable_npc = $InteractableNpc
@@ -18,4 +20,4 @@ func _on_line_edit_text_submitted(new_text):
 	if self._button.disabled:
 		self._button.disabled = false
 		
-	ChatManager.ApiKey = self._line_edit.text
+	chatManager.ApiKey = self._line_edit.text
