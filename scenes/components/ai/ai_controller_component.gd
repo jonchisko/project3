c9d2b3a0c9b98e65b897b4679dcca6950f2b106f
Jon Škoberne <jonchisko@gmail.com>
2025-03-26 23:31:37 +0100

    Enemy chasing and attacking

--- scenes/components/ai/ai_controller_component.gd
+++ scenes/components/ai/ai_controller_component.gd
@@ -1,22 +1,20 @@
 extends Node2D
 class_name AiController
 
-# Patrolling, Searching, Chasing
-# Patrolling -> Random locations withing some radius from given point. Go there, wait 1-3 secs, go next
-# Searching -> when player out of detection range, go from chasing to searching -> go to
-# last known position, wait there for 1-3 seconds and start patrolling
-# chasing -> when player within detection range, start chasing to some distance, stay at that distance
-# attacking done by the main script
 
-enum BehaviourStat {
-	Patrol,
-	Search,
-	Chase,
-}
+signal move_to(location: Vector2)
+
+
+@export var update_frequency: float = 0.2
+
+@onready var _patrol_timer: Timer = $PatrolTimer
+@onready var _player_detector: Area2D = $PlayerDetector
+
 
 var _player_reference: Node2D = null
-var _current_behaviour: BehaviourStat = BehaviourStat.Patrol
 var _patrol_origin: Vector2 = Vector2.ZERO
+var _check_visibility: bool
+var _stop_chase: bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,8 +24,43 @@ func _ready() -> void:
 		printerr("Player reference could not be obtained in AiControllerComponent.")
 	
 	self._patrol_origin = self.global_position
+	self._check_visibility = false
+	self._patrol_timer.wait_time = self.update_frequency
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_player_detector_area_entered(area: Area2D) -> void:
+	if not area.get_parent() is Player:
+		return
+		
+	self._check_visibility = true
+	
+	
+func _on_player_detector_area_exited(area: Area2D) -> void:
+	if not area.get_parent() is Player:
+		return
+		
+	self._check_visibility = false
+	
+	
+func _is_visible() -> bool:
+	var space_state = self.get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(self.global_position, self._player_reference.global_position, 
+		2, [self])
+		
+	var result = space_state.intersect_ray(query)
+	print(result)
+	return result.is_empty()
+	
+
+
+func _on_patrol_timer_timeout() -> void:
+	if not self._stop_chase and self._check_visibility and self._is_visible():
+		self.move_to.emit(self._player_reference.global_position)
+
+
+func _on_stop_chasing_area_area_entered(area: Area2D) -> void:
+	self._stop_chase = true
+
+
+func _on_stop_chasing_area_area_exited(area: Area2D) -> void:
+	self._stop_chase = false
--- scenes/game_objects/npcs/enemies/knight/knight.gd
+++ scenes/game_objects/npcs/enemies/knight/knight.gd
@@ -15,10 +15,15 @@ enum AttackType {
 @onready var _health_component: HealthComponent = $HealthComponent
 @onready var _attack_timer_cd: Timer = $AttackTimerCd
 @onready var _damage_area: DamageArea = $DamageArea
+@onready var _navigation_agent_2d: NavigationAgent2D = $Navigation/NavigationAgent2D
+
 
 
 var _current_attack_type_index: int = 0
-
+var _attack_on_cd: bool = false
+var _searching: bool = false
+var _attacking: bool = false
+var _player_in_attack_range: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,15 +34,26 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Vector2.ZERO
+	if self._searching:
+		direction = (-self.global_position + self._navigation_agent_2d.get_next_path_position()).normalized()
 	self._velocity_component.move_in_direction(self, direction)
-	if self._attack_timer_cd.is_stopped():
-		print(self._attack_timer_cd.is_stopped())
-		var attack_type = self._get_next_attack_type()
-		self._attack(attack_type)
+	
+	#print(self.velocity)
+	if not self._attacking:
+		if self.velocity.x != 0 or self.velocity.y != 0:
+			self._animation_player.play("running")
+		else:
+			self._animation_player.play("idle")
+	
+	#if self.velocity.x < 0:
+		#self.scale.x = -1
+	#if self.velocity.x > 0:
+		#self.scale.x = 1
 
 
 func _attack(attack_type: AttackType) -> void:
-	print("attack")
+	self._attacking = true
+	self._attack_on_cd = true
 	match attack_type:
 		AttackType.Basic:
 			self._damage_area.damage_amount = 5
@@ -45,14 +61,11 @@ func _attack(attack_type: AttackType) -> void:
 		AttackType.Special: 
 			self._damage_area.damage_amount = 15
 			self._animation_player.play("attack_swing")
-	print("wait")
 	await self._animation_player.animation_finished
-	print("idle")
-	self._animation_player.play("idle")
 	
-	self._attack_timer_cd.wait_time = 1
+	self._attacking = false
+	self._attack_timer_cd.wait_time = 1.5
 	self._attack_timer_cd.start()
-	print("start")
 
 
 func _get_next_attack_type() -> AttackType:
@@ -63,15 +76,26 @@ func _get_next_attack_type() -> AttackType:
 
 
 func _on_attack_range_area_entered(area):
+	print("In attack range")
 	if not area.get_parent() is Player:
 		return
-		
-	if self._attack_timer_cd.is_stopped():
+	
+	self._player_in_attack_range = true
+	
+	if not self._attack_on_cd:
+		print("Enemy attacks")
 		var attack_type = self._get_next_attack_type()
 		self._attack(attack_type)
 
 
+func _on_attack_range_area_exited(area: Area2D) -> void:
+	if not area.get_parent() is Player:
+		return
+	self._player_in_attack_range = false
+	
+
 func _on_damagable_damage_detected(amount):
+	self._animation_player.play("hit")
 	self._health_component.damage(amount)
 
 
@@ -82,3 +106,19 @@ func _on_health_component_death():
 	$AttackRange/CollisionShape2D.set_deferred("disabled", true)
 	
 	self._animation_player.play("death")
+
+
+func _on_attack_timer_cd_timeout() -> void:
+	self._attack_on_cd = false
+	if self._player_in_attack_range:
+		var attack_type = self._get_next_attack_type()
+		self._attack(attack_type)
+
+
+func _on_ai_controller_component_move_to(location: Vector2) -> void:
+	self._searching = true
+	self._navigation_agent_2d.target_position = location
+
+
+func _on_navigation_agent_2d_navigation_finished() -> void:
+	self._searching = false
