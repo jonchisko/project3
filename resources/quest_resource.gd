599cfe2743d3e2a19e2513e0f885c213cbf89cc6
Jon Škoberne <jonchisko@gmail.com>
2025-04-21 11:24:54 +0200

    Add quest data

--- resources/quest_resource.gd
+++ resources/quest_resource.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name QuestResource
 
 @export var id: String = ""
+@export var primary_quest: bool = false
 @export var title: String = ""
 @export var description: String = ""
 @export var condition_expression: String = ""
