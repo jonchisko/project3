d4d7a54ba0c6632967e04d80c96606e734ecde7d
Jon Škoberne <jonchisko@gmail.com>
2025-06-08 19:01:51 +0200

    Adjusting options and settings

--- scenes/autoloads/open_ai_configuration.gd
+++ scenes/autoloads/open_ai_configuration.gd
@@ -2,5 +2,25 @@ extends Node
 
 
 var open_ai_api_key: String = ""
-var open_ai_model: String = ""
+var open_ai_model: OpenAiTypes.ModelVersion
 var template_type: OpenAiTypes.TemplateType
+
+var temperature: float = 0.5:
+	set(value):
+		temperature = clampf(value, 0.0, 1.0)
+		
+var frequency_penalty: float = 0.0:
+	set(value):
+		frequency_penalty = clampf(value, -2.0, 2.0)
+
+var history_max_last_exchanges: int = 5:
+	set(value):
+		history_max_last_exchanges = clamp(value, 1, 10)
+		
+var history_max_similar_results: int = 10:
+	set(value):
+		history_max_similar_results = clamp(value, 1, 20)
+		
+var history_threshold_similar_results: float = 0.6:
+	set(value):
+		history_threshold_similar_results = clampf(value, 0.0, 1.0)
--- scenes/ui/menu/options_menu.gd
+++ scenes/ui/menu/options_menu.gd
@@ -12,6 +12,7 @@ signal options_closed
 @onready var _screen_mode_button = %ScreenModeButton
 @onready var open_ai_api_text_edit = %OpenAiApiTextEdit
 @onready var gpt_template_button: OptionButton = %GptTemplateButton
+@onready var model_button: OptionButton = %ModelButton
 
 
 const _WINDOW_ID: int = 0
@@ -33,9 +34,18 @@ func _update_options():
 	
 	open_ai_api_text_edit.text = OpenAiConfiguration.open_ai_api_key
 	
-	gpt_template_button.select(self._from_template_type_to_index(OpenAiConfiguration.template_type))
+	self.gpt_template_button.select(self._from_template_type_to_index(OpenAiConfiguration.template_type))
 	self._update_open_ai_based_on_selection(self._from_template_type_to_index(OpenAiConfiguration.template_type))
-
+	
+	self.model_button.select(self._from_model_type_to_index(OpenAiConfiguration.open_ai_model))
+	self._update_open_ai_model_based_on_selection(self._from_model_type_to_index(OpenAiConfiguration.open_ai_model))
+	
+	%TemperatureInput.text = str(OpenAiConfiguration.temperature)
+	%FrequencyPenaltyInput.text = str(OpenAiConfiguration.frequency_penalty)
+	%HistoryMaxExchanges.text = str(OpenAiConfiguration.history_max_last_exchanges)
+	%MaxSimilarResults.text = str(OpenAiConfiguration.history_max_similar_results)
+	%SimilarResults.text = str(OpenAiConfiguration.history_threshold_similar_results)
+	
 
 func _on_tab_container_tab_clicked(tab):
 	$AudioStreamPlayer.play()
@@ -103,7 +113,7 @@ func _on_gpt_template_button_item_selected(index: int) -> void:
 
 
 func _update_open_ai_based_on_selection(index: int) -> void:
-	match gpt_template_button.get_item_text(index):
+	match self.gpt_template_button.get_item_text(index):
 		"Strict":
 			OpenAiConfiguration.template_type = OpenAiTypes.TemplateType.Strict
 		"Loose":
@@ -121,3 +131,65 @@ func _from_template_type_to_index(template_type) -> int:
 			return 1
 		_:
 			return 0
+
+
+func _on_model_button_item_selected(index: int) -> void:
+	self._update_open_ai_model_based_on_selection(index)
+
+
+func _update_open_ai_model_based_on_selection(index: int) -> void:
+	match self.model_button.get_item_text(index):
+		"model-3-5-turbo":
+			OpenAiConfiguration.open_ai_model = OpenAiTypes.ModelVersion.Gpt_3_5_Turbo
+		"model-4-turbo":
+			OpenAiConfiguration.open_ai_model = OpenAiTypes.ModelVersion.Gpt_4_Turbo
+		"model-4o":
+			OpenAiConfiguration.open_ai_model = OpenAiTypes.ModelVersion.Gpt_4o
+		"model-4-1":
+			OpenAiConfiguration.open_ai_model = OpenAiTypes.ModelVersion.Gpt_4_1
+		"model-4-1-mini":
+			OpenAiConfiguration.open_ai_model = OpenAiTypes.ModelVersion.Gpt_4_1_Mini
+
+
+func _from_model_type_to_index(model_type) -> int:
+	if model_type == null:
+		return 0
+	
+	match model_type:
+		OpenAiTypes.ModelVersion.Gpt_3_5_Turbo:
+			return 0
+		OpenAiTypes.ModelVersion.Gpt_4_Turbo:
+			return 1
+		OpenAiTypes.ModelVersion.Gpt_4o:
+			return 2
+		OpenAiTypes.ModelVersion.Gpt_4_1:
+			return 3
+		OpenAiTypes.ModelVersion.Gpt_4_1_Mini:
+			return 4
+		_:
+			return 0
+
+
+func _on_temperature_input_text_submitted(new_text: String) -> void:
+	var float_value = new_text.to_float()
+	OpenAiConfiguration.temperature = float_value
+
+
+func _on_frequency_penalty_input_text_submitted(new_text: String) -> void:
+	var float_value = new_text.to_float()
+	OpenAiConfiguration.frequency_penalty = float_value
+
+
+func _on_history_max_exchanges_text_submitted(new_text: String) -> void:
+	var int_value = new_text.to_int()
+	OpenAiConfiguration.history_max_last_exchanges = int_value
+
+
+func _on_max_similar_results_text_submitted(new_text: String) -> void:
+	var int_value = new_text.to_int()
+	OpenAiConfiguration.history_max_similar_results = int_value
+
+
+func _on_similar_results_text_submitted(new_text: String) -> void:
+	var float_value = new_text.to_float()
+	OpenAiConfiguration.history_threshold_similar_results = float_value
