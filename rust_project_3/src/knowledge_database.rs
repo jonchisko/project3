1b7fa60cefb28dc4a8a223c3744625a7ffc5f160
Jon Škoberne <jonchisko@gmail.com>
2025-08-12 23:58:52 +0200

    Add more functionality to sqlite part of knowledge node

--- rust_project_3/src/knowledge_database.rs
+++ rust_project_3/src/knowledge_database.rs
@@ -1,13 +1,33 @@
 use std::fmt::Display;
 
-use godot::{classes::Engine, prelude::*};
-use rusqlite::{Connection, Result};
+use godot::prelude::*;
+use rusqlite::{Connection, Result, params};
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+#[derive(GodotConvert, Debug)]
+#[godot(via = i64)]
+pub enum GameObject {
+    Entity,
+    Item,
+}
+
+impl Display for GameObject {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let enum_display = match self {
+            GameObject::Entity => "entity",
+            GameObject::Item => "item",
+        };
+
+        f.pad(enum_display)
+    }
+}
+
+impl From<GameObject> for i64 {
+    fn from(value: GameObject) -> Self {
+        match value {
+            GameObject::Entity => 0,
+            GameObject::Item => 1,
+        }
+    }
 }
 
 #[derive(GodotConvert, Debug)]
@@ -141,7 +161,8 @@ impl INode for KnowledgeDatabase {
                 id                  INTEGER PRIMARY KEY,
                 action_id           INTEGER NOT NULL,
                 source_entity_id    INTEGER NOT NULL,
-                target_entity_id    INTEGER NOT NULL,
+                target_object_id    INTEGER NOT NULL,
+                target_object_type  INTEGER NOT NULL,
                 time                REAL NOT NULL,
                 FOREIGN KEY (action_id)
                 REFERENCES actions (action_id)
@@ -151,10 +172,7 @@ impl INode for KnowledgeDatabase {
                 REFERENCES entities (entity_id)
                     ON UPDATE CASCADE
                     ON DELETE RESTRICT,
-                FOREIGN KEY (target_entity_id)
-                REFERENCES entities (entity_id)
-                    ON UPDATE CASCADE
-                    ON DELETE RESTRICT
+                CHECK (target_object_type == 0 OR target_object_type == 1)
             )",
                 (),
             )
@@ -171,39 +189,154 @@ impl INode for KnowledgeDatabase {
 impl KnowledgeDatabase {
     #[func]
     fn set_quest_done(&mut self, quest_id: String) -> bool {
-        todo!()
+        let update_result = self.connection.execute(
+            "UPDATE quests SET done = 1 WHERE game_quest_id = ?1",
+            [quest_id],
+        );
+
+        match update_result {
+            Ok(num_rows) => {
+                godot_print!("Updated quest, rows affected: {}", num_rows);
+                true
+            }
+            Err(_) => {
+                godot_error!("Could not update quest");
+                false
+            }
+        }
     }
 
     #[func]
     fn get_quest_triplets(&self) -> String {
-        todo!()
+        let mut stmt = self
+            .connection
+            .prepare("SELECT game_quest_id, done FROM quests")
+            .expect("Failed at creating prepared statement for quest triplets");
+
+        let triplets_iter = stmt
+            .query_map([], |row| {
+                let game_quest_id: String = row
+                    .get(0)
+                    .expect("Could not get 'game_quest_id' from quests table");
+                let done: i64 = row.get(1).expect("Could not get 'done' from quests table");
+
+                if done == 1 {
+                    Ok(format!("player completes {}", game_quest_id))
+                } else {
+                    Ok(format!("player not_completes {}", game_quest_id))
+                }
+            })
+            .expect("Iterator could not be constructed");
+
+        triplets_iter
+            .filter(|triplet| triplet.is_ok())
+            .fold("".to_string(), |mut acc, triplet| {
+                acc.push_str(&triplet.unwrap());
+                acc.push('\n');
+                acc
+            })
     }
 
     #[func]
-    fn update_possession_quantity(
+    fn update_ownership_quantity(
         &mut self,
-        item_id: String,
-        entity_id: String,
-        new_value: i32,
+        game_item_id: String,
+        game_entity_id: String,
+        amount: i32,
     ) -> bool {
-        // If exists, update, if it does not, insert
+        let (item, entity) = (
+            self.get_item_id(game_item_id),
+            self.get_entity_id(game_entity_id),
+        );
 
+        if let Err(_) = item {
+            godot_error!("Item not found");
+            return false;
+        }
+
+        if let Err(_) = entity {
+            godot_error!("Entity not found");
+            return false;
+        }
+
+        let (item, entity) = (item.unwrap(), entity.unwrap());
+
+        let update_result = self.connection.execute(
+            "UPDATE ownership SET amount = ?1 WHERE item_id = ?2 AND entity_id = ?3",
+            params![amount, item, entity],
+        );
+
+        if let Err(_) = update_result {
+            godot_error!("Could not update ownership");
+            return false;
+        }
+
+        let num_rows = update_result.unwrap();
+
+        if num_rows == 0 {
+            godot_print!("No update, inserting new ownership data");
+            let insert_result = self.connection.execute(
+                "INSERT INTO ownership (item_id, entity_id, amount) VALUES (?1, ?2, ?3)",
+                params![item, entity, amount],
+            );
+            if let Err(_) = insert_result {
+                godot_error!("Insertion failed");
+                return false;
+            }
+        } else {
+            godot_print!("Updated ownership, rows affected: {}", num_rows);
+        }
+
+        true
+    }
+
+    #[func]
+    fn get_ownership_quantity(&self, game_item_id: String, game_entity_id: String) -> i64 {
+        let (item, entity) = (
+            self.get_item_id(game_item_id),
+            self.get_entity_id(game_entity_id),
+        );
+
+        if let Err(_) = item {
+            godot_error!("Item not found");
+            return -1;
+        }
+
+        if let Err(_) = entity {
+            godot_error!("Entity not found");
+            return -1;
+        }
+
+        let (item, entity) = (item.unwrap(), entity.unwrap());
+
+        let entity: Result<i64> = self.connection.query_one(
+            "SELECT amount FROM ownership WHERE item_id = ?1 AND entity_id = ?2",
+            [item, entity],
+            |row| row.get(0),
+        );
+
+        match entity {
+            Ok(val) => val,
+            Err(_) => {
+                godot_error!("Error obtaining amount");
+                -1
+            }
+        }
+    }
+
+    #[func]
+    fn get_ownership_triplets(&self) -> String {
         todo!()
     }
 
     #[func]
-    fn get_possession_quantity(&self, item_id: String, entity_id: String) -> i32 {
-        // if it does not exist, return 0
-        todo!()
-    }
-
-    #[func]
-    fn get_possessions_triplets(&self) -> String {
-        todo!()
-    }
-
-    #[func]
-    fn add_action(&mut self, action: String, source: String, target: String) -> bool {
+    fn add_action(
+        &mut self,
+        action: String,
+        source: String,
+        target: String,
+        target_type: GameObject,
+    ) -> bool {
         todo!()
     }
 
@@ -298,5 +431,25 @@ impl KnowledgeDatabase {
             .expect("Error when inserting game action name into actions");
         stmt.execute([GameAction::Wears.to_string()])
             .expect("Error when inserting game action name into actions");
+    }
+
+    fn get_item_id(&self, game_item_id: String) -> Result<i64, String> {
+        self.connection
+            .query_one(
+                "SELECT item_id FROM items WHERE game_item_id = ?1",
+                [game_item_id],
+                |row| row.get(0),
+            )
+            .map_err(|err| format!("Item id could not be obtained, {}", err.to_string()))
+    }
+
+    fn get_entity_id(&self, game_entity_id: String) -> Result<i64, String> {
+        self.connection
+            .query_one(
+                "SELECT entity_id FROM items WHERE game_entity_id = ?1",
+                [game_entity_id],
+                |row| row.get(0),
+            )
+            .map_err(|err| format!("Entity id could not be obtained, {}", err.to_string()))
     }
 }
