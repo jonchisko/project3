8b65b5de004f0e54a0a389d608ae05215858d0c7
Jon Škoberne <jonchisko@gmail.com>
2026-01-22 16:48:35 +0100

    feat: adjust npc descriptions and add outpost 'lock'

--- scripts/outpost_area_2d.gd
+++ scripts/outpost_area_2d.gd
@@ -3,12 +3,25 @@ extends Area2D
 @export var current_scene: SceneSyncher
 @export_file("*.tscn") var new_scene
 
+
+@onready var label_no_access: Label = $TargetNode/LabelNoAccess
+@onready var label_allowed: Label = $TargetNode/LabelAllowed
+
+
 var _changing_scene: bool = false
 var _near_outpost: bool = false
 
+
+var _player_inventory: InventoryManager
+
+
+func _ready() -> void:
+	self._player_inventory = self.get_tree().get_first_node_in_group("player").find_child("InventoryManager") as InventoryManager
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if self._near_outpost and Input.is_action_just_pressed("interact") and not self._changing_scene:
+	if self._near_outpost and self._player_inventory.has_item("outpost_keycode", 1) and Input.is_action_just_pressed("interact") and not self._changing_scene:
 		print("OutpostArea2D: Changing scene")
 		GameEvents.level_change.emit(GameTypes.LevelChangeType.OffLoading)
 		self._changing_scene = true
@@ -17,9 +30,15 @@ func _process(delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	self._near_outpost = true
-	$Label.visible = true
+	if self._player_inventory.has_item("outpost_keycode", 1):
+		self.label_allowed.visible = true
+	else:
+		self.label_no_access.visible = true
 
 
 func _on_area_exited(area: Area2D) -> void:
 	self._near_outpost = false
-	$Label.visible = false
+	if self.label_allowed.visible:
+		self.label_allowed.visible = false
+	if self.label_no_access.visible:
+		self.label_no_access.visible = false
